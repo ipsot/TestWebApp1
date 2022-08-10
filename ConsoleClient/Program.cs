d6f5f3470cc910c8872e3c755c84a6f27c15548e
ipsot <76401884+ipsot@users.noreply.github.com>
2022-08-10 13:32:01 +0300

    Add files via upload

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ConsoleClient
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter");
+            Console.ReadKey();
+
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+
+            string jsonData = webClient.DownloadString("https://localhost:44321/api/test/getperson");
+            Person person = JsonConvert.DeserializeObject<Person>(jsonData);
+
+            Console.WriteLine($"Name:{person.Name} Age:{person.Age}");
+            Console.ReadKey();
+        }
+    }
+}
