649daf6b8fb6ff773376d84193d734fa2950bd52
ipsot <76401884+ipsot@users.noreply.github.com>
2022-08-10 13:28:33 +0300

    Add files via upload

---
+++ WebClient/script.js
@@ -0,0 +1,20 @@
+﻿let nameField = document.getElementById("name");
+let ageField = document.getElementById("age");
+
+async function GetPersonOnClick()
+{
+    alert("hello");
+    let response = await fetch("https://localhost:44321/api/test/getperson");
+
+    if (response.ok)
+    {
+        let json = await response.json();
+        let data = JSON.parse(json);
+
+        nameField.innerHTML = data.Name;
+        ageField.innerHTML = data.Age;
+    }
+    else {
+        alert("HTML Error" + response.status);
+    }
+}
---
+++ WebClient2/Views/Client/script.js
@@ -0,0 +1,19 @@
+﻿let nameField = document.getElementById("name");
+let ageField = document.getElementById("age");
+
+async function GetPersonOnClick()
+{
+    alert("hello");
+    let response = await fetch("https://localhost:44321/api/test/getperson");
+
+    if (response.ok)
+    {
+        let json = await response.json();
+
+        nameField.innerHTML = json.Name;
+        ageField.innerHTML = json.Age;
+    }
+    else {
+        alert("HTML Error" + response.status);
+    }
+}
